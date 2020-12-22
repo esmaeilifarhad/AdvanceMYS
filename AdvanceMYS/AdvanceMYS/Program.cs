492b0e77d2204aac25785736bf879002eb5eb06c
Farhad Esmaeili <esmaili.farhad67@gmail.com>
2020-12-22 14:42:52 +0330

    13991002 14:42

---
+++ AdvanceMYS/AdvanceMYS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvanceMYS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
